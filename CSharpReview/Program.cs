356e535642748df6b36eb01e2cd8e526f71f8a11
Alex8711 <liuzhendell@gmail.com>
2020-10-19 09:52:30 -0600

    add some  code

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -4,9 +4,17 @@ namespace CSharpReview
 {
     class Program
     {
+        // Enter point of programms in C#
         static void Main(string[] args)
         {
+            string name;
+            // input from the console
+            name = Console.ReadLine();
+            
+            // output to the console 
             Console.WriteLine("Hello World! What a nice day! Today");
+
+            // Exit point of programs in C#
         }
     }
 }
